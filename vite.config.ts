7e039104c46a90c34df700df40aa45feb682a30b
moaz25435-ops <moaz25435@gmail.com>
2026-09-19 00:20:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './'
+  base: '/simulate-learn-flow/',
   plugins: [react(), vlyPlugin(), tailwindcss()],
   resolve: {
     alias: {
